2019b2453fef117b91c8fa563998477577f268e9
kokweikhong <kokweikhong@gmail.com>
2023-10-31 23:56:21 +0800

    updates

--- khongfamily-app/components/finance/FinanceExpensesChart.tsx
+++ khongfamily-app/components/finance/FinanceExpensesChart.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { FinanceExpensesSummary } from "@/types/finance";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
